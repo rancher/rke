c6ebf42c43ceeccc26d1980aabc4180b196cd53d
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2018-01-15 19:51:44 +0200

    remove artifacts images

--- package/certs-deployer/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ACCT=${ACCT:-rancher}
-
-docker build -t $ACCT/rke-cert-deployer:0.1.1 .
-docker push $ACCT/rke-cert-deployer:0.1.1
--- package/nginx-proxy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run confd
-confd -onetime -backend env
-
-# Start nginx
-nginx -g 'daemon off;'
