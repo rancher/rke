85b15d6da40b114f7f507b3dfe9dfc63fd070721
moelsayed <m.elsayed@gmail.com>
2018-05-09 23:49:36 +0200

    vendor update

--- vendor/github.com/rancher/types/apis/management.cattle.io/v3/rke_types.go
+++ vendor/github.com/rancher/types/apis/management.cattle.io/v3/rke_types.go
@@ -35,8 +35,8 @@ type RancherKubernetesEngineConfig struct {
 	CloudProvider CloudProvider `yaml:"cloud_provider" json:"cloudProvider,omitempty"`
 	// kubernetes directory path
 	PrefixPath string `yaml:"prefix_path" json:"prefixPath,omitempty"`
-	// Number of status check retries for addon deployment jobs
-	AddonJobRetries int `yaml:"addon_job_retries" json:"addonJobRetries,omitempty" norman:"default=5"`
+	// Timeout in seconds for status check on addon deployment jobs
+	AddonJobTimeout int `yaml:"addon_job_timeout" json:"addonJobTimeout,omitempty" norman:"default=30"`
 	// Bastion/Jump Host configuration
 	BastionHost BastionHost `yaml:"bastion_host" json:"bastionHost,omitempty"`
 }
