9c172c40b2b46f50ff045dac0ffa102883b7cb09
moelsayed <m.elsayed@gmail.com>
2018-08-25 20:57:07 +0200

    Add 1.11 supported docker versions

--- docker/docker.go
+++ docker/docker.go
@@ -34,6 +34,7 @@ var K8sDockerVersions = map[string][]string{
 	"1.8":  {"1.11.x", "1.12.x", "1.13.x", "17.03.x"},
 	"1.9":  {"1.11.x", "1.12.x", "1.13.x", "17.03.x"},
 	"1.10": {"1.11.x", "1.12.x", "1.13.x", "17.03.x"},
+	"1.11": {"1.11.x", "1.12.x", "1.13.x", "17.03.x"},
 }
 
 func DoRunContainer(ctx context.Context, dClient *client.Client, imageCfg *container.Config, hostCfg *container.HostConfig, containerName string, hostname string, plane string, prsMap map[string]v3.PrivateRegistry) error {
