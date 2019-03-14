8de1c342a1028b154c58ec56fd2d688e6361109d
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-03-14 04:33:08 +0200

    Restart Cattle agent pods and kubeapi auth pods

--- cluster/addons.go
+++ cluster/addons.go
@@ -32,6 +32,9 @@ const (
 	KubeDNSAddonAppName            = "kube-dns"
 	KubeDNSAutoscalerAppName       = "kube-dns-autoscaler"
 	CoreDNSAutoscalerAppName       = "coredns-autoscaler"
+	KubeAPIAuthAppName             = "kube-api-auth"
+	CattleClusterAgentAppName      = "cattle-cluster-agent"
+	CattleAgentAppName             = "cattle-agent"
 
 	CoreDNSProvider = "coredns"
 	KubeDNSProvider = "kube-dns"
