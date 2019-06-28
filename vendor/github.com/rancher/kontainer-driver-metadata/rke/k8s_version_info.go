bc611f3f100e2befa31e08000cd7da3bd641e195
kinarashah <kinara@rancher.com>
2019-06-27 21:03:32 -0700

    vendor change

--- vendor/github.com/rancher/kontainer-driver-metadata/rke/k8s_version_info.go
+++ vendor/github.com/rancher/kontainer-driver-metadata/rke/k8s_version_info.go
@@ -39,13 +39,13 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MaxRancherVersion: "2.2",
 			MaxRKEVersion:     "0.2.2",
 		},
-		"v1.11": {
-			MaxRancherVersion: "2.2",
-			MaxRKEVersion:     "0.2.2",
-		},
-		"v1.15.0-rancher1-1": {
-			MinRancherVersion: "2.3",
-		},
+		//"v1.11": {
+		//	MaxRancherVersion: "2.2",
+		//	MaxRKEVersion:     "0.2.2",
+		//},
+		//"v1.15.0-rancher1-1": {
+		//	MinRancherVersion: "2.3",
+		//},
 		"v1.8.10-rancher1-1": {
 			DeprecateRKEVersion:     "0.2.2",
 			DeprecateRancherVersion: "2.2",
