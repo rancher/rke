b5d7f5dcd4dd1d936ec3d71772a28e4448e3cbe3
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-08-08 23:31:18 +0200

    Handle changing public ip for etcd member delete

--- hosts/tunnel.go
+++ hosts/tunnel.go
@@ -3,10 +3,12 @@ package hosts
 import (
 	"context"
 	"fmt"
-	"github.com/rancher/rke/metadata"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/rancher/rke/metadata"
 
 	"net"
 
@@ -34,7 +36,11 @@ func (h *Host) TunnelUp(ctx context.Context, dialerFactory DialerFactory, cluste
 	}
 	// set Docker client
 	logrus.Debugf("Connecting to Docker API for host [%s]", h.Address)
-	h.DClient, err = client.NewClient("unix:///var/run/docker.sock", DockerAPIVersion, httpClient, nil)
+	h.DClient, err = client.NewClientWithOpts(
+		client.WithHost("unix:///var/run/docker.sock"),
+		client.WithVersion(DockerAPIVersion),
+		client.WithHTTPClient(httpClient),
+		client.WithTimeout(10*time.Second))
 	if err != nil {
 		return fmt.Errorf("Can't initiate NewClient: %v", err)
 	}
