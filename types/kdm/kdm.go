987a3bf618fd0e6584596ff496bee0ced7c686d9
Menna <menna.elmasry@rancher.com>
2020-07-21 22:41:30 +0200

    add comment

--- types/kdm/kdm.go
+++ types/kdm/kdm.go
@@ -48,6 +48,7 @@ type Data struct {
 
 	// K3S specific data, opaque and defined by the config file in kdm
 	K3S  map[string]interface{} `json:"k3s,omitempty"`
+	//Rke2 specific data, defined by the config file in kdm
 	RKE2 map[string]interface{} `json:"rke2,omitempty"`
 }
 
