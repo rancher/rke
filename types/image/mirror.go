00405193fd8941f017df4d6412e6e0223200b53c
Ricardo Weir <ricardo.weir1@gmail.com>
2021-03-08 15:24:48 -0700

    Update to use mirrored- prefix

--- types/image/mirror.go
+++ types/image/mirror.go
@@ -10,20 +10,19 @@ func Mirror(image string) string {
 		return image
 	}
 
-	image = strings.Replace(image, "gcr.io/google_containers", "rancher", 1)
-	image = strings.Replace(image, "quay.io/coreos/", "rancher/coreos-", 1)
-	image = strings.Replace(image, "quay.io/calico/", "rancher/calico-", 1)
-	image = strings.Replace(image, "plugins/docker", "rancher/plugins-docker", 1)
-	image = strings.Replace(image, "k8s.gcr.io/defaultbackend", "rancher/nginx-ingress-controller-defaultbackend", 1)
-	image = strings.Replace(image, "k8s.gcr.io/k8s-dns-node-cache", "rancher/k8s-dns-node-cache", 1)
-	image = strings.Replace(image, "plugins/docker", "rancher/plugins-docker", 1)
-	image = strings.Replace(image, "kibana", "rancher/kibana", 1)
-	image = strings.Replace(image, "jenkins/", "rancher/jenkins-", 1)
+	image = strings.Replace(image, "gcr.io/google_containers/", "rancher/mirrored-", 1)
+	image = strings.Replace(image, "quay.io/coreos/", "rancher/mirrored-coreos-", 1)
+	image = strings.Replace(image, "quay.io/calico/", "rancher/mirrored-calico-", 1)
+	image = strings.Replace(image, "plugins/docker", "rancher/mirrored-plugins-docker", 1)
+	image = strings.Replace(image, "k8s.gcr.io/defaultbackend", "rancher/mirrored-nginx-ingress-controller-defaultbackend", 1)
+	image = strings.Replace(image, "k8s.gcr.io/k8s-dns-node-cache", "rancher/mirrored-k8s-dns-node-cache", 1)
+	image = strings.Replace(image, "kibana", "rancher/mirrored-kibana", 1)
+	image = strings.Replace(image, "jenkins/", "rancher/mirrored-jenkins-", 1)
 	image = strings.Replace(image, "alpine/git", "rancher/alpine-git", 1)
-	image = strings.Replace(image, "prom/", "rancher/prom-", 1)
-	image = strings.Replace(image, "quay.io/pires", "rancher", 1)
-	image = strings.Replace(image, "coredns/", "rancher/coredns-", 1)
-	image = strings.Replace(image, "minio/", "rancher/minio-", 1)
+	image = strings.Replace(image, "prom/", "rancher/mirrored-prom-", 1)
+	image = strings.Replace(image, "quay.io/pires/", "rancher/mirrored-", 1)
+	image = strings.Replace(image, "coredns/", "rancher/mirrored-coredns-", 1)
+	image = strings.Replace(image, "minio/", "rancher/mirrored-minio-", 1)
 
 	Mirrors[image] = orig
 	return image
