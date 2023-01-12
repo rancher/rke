4558c4b74af5269437496af01a4407e246c1dfd9
Masashi Honma <masashi.honma@gmail.com>
2023-01-13 08:12:13 +0900

    Set appropriate value to warning message
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- services/etcd.go
+++ services/etcd.go
@@ -463,7 +463,7 @@ func RunEtcdSnapshotSave(ctx context.Context, etcdHost *hosts.Host, prsMap map[s
 		status, _, stderr, err := docker.GetContainerOutput(ctx, etcdHost.DClient, EtcdSnapshotOnceContainerName, etcdHost.Address, false)
 		if status != 0 || err != nil {
 			if removeErr := docker.RemoveContainer(ctx, etcdHost.DClient, etcdHost.Address, EtcdSnapshotOnceContainerName); removeErr != nil {
-				log.Warnf(ctx, "[etcd] Failed to remove container [%s] on host [%s]: %v", EtcdSnapshotOnceContainerName, removeErr, etcdHost.Address)
+				log.Warnf(ctx, "[etcd] Failed to remove container [%s] on host [%s]: %v", EtcdSnapshotOnceContainerName, etcdHost.Address, removeErr)
 			}
 			if err != nil {
 				return err
