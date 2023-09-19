eaea04fd85c0eefddf695c0f260c89adfc393d0d
vardhaman22 <vardhaman.surana@suse.com>
2023-09-19 08:16:15 +0530

    update rke dev version to v1.5.99

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -90,7 +90,7 @@ func readFile(file string) ([]byte, error) {
 	return os.ReadFile(file)
 }
 
-const RKEVersionDev = "v1.4.99"
+const RKEVersionDev = "v1.5.99"
 
 func initAddonTemplates(data kdm.Data) {
 	K8sVersionToTemplates = data.K8sVersionedTemplates
