ebde95b72ab0f7b10c9e15b0a0ed14fe5bcb4cfc
Jiaqi Luo <6218999+jiaqiluo@users.noreply.github.com>
2024-03-27 11:42:24 -0700

    switch KDM branch to release-v2.8

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	defaultURL = "https://releases.rancher.com/kontainer-driver-metadata/dev-v2.8/data.json"
+	defaultURL = "https://releases.rancher.com/kontainer-driver-metadata/release-v2.8/data.json"
 	dataFile   = "data/data.json"
 )
 
