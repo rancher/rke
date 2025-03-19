41194151585b955cfbe8b91ece434f37abce6753
Jake Hyde <jakefhyde@gmail.com>
2025-03-19 12:53:29 -0400

    Update KDM release URL to release-v2.11

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	defaultDevURL     = "https://releases.rancher.com/kontainer-driver-metadata/dev-v2.11/data.json"
-	defaultReleaseURL = "https://releases.rancher.com/kontainer-driver-metadata/release-v2.10/data.json"
+	defaultReleaseURL = "https://releases.rancher.com/kontainer-driver-metadata/release-v2.11/data.json"
 	dataFile          = "data/data.json"
 )
 
