27273b5caf064598619ca2b8886756bee61b3e3f
Kinara Shah <kinara.shah@suse.com>
2025-04-17 11:13:08 -0700

    Revert "use sha256 for checksumming when k8s version >= 1.31.6"
    This reverts commit 740239cb194146f5957bd041af8c6dc9954ded44.

--- cluster/plan_test.go
+++ cluster/plan_test.go
@@ -1,9 +1,6 @@
 package cluster
 
 import (
-	"crypto/md5"
-	"crypto/sha256"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -50,60 +47,6 @@ func Test_getUniqStringList(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equalf(t, tt.want, getUniqStringList(tt.args.l), "getUniqStringList(%v)", tt.args.l)
-		})
-	}
-}
-
-func Test_getStringChecksum(t *testing.T) {
-	tests := []struct {
-		name     string
-		config   string
-		version  string
-		expected string
-	}{
-		{
-			name:     "version greater than 1.31.6, use sha256",
-			config:   "test-config",
-			version:  "v1.32.0-rancher0",
-			expected: fmt.Sprintf("%x", sha256.Sum256([]byte("test-config"))),
-		},
-		{
-			name:     "version exactly 1.31.6, use sha256",
-			config:   "test-config",
-			version:  "v1.31.6-rancher0",
-			expected: fmt.Sprintf("%x", sha256.Sum256([]byte("test-config"))),
-		},
-		{
-			name:     "version exactly 1.31.0, use md5",
-			config:   "test-config",
-			version:  "v1.31.0-rancher0",
-			expected: fmt.Sprintf("%x", md5.Sum([]byte("test-config"))),
-		},
-		{
-			name:     "version less than 1.31, use md5",
-			config:   "test-config",
-			version:  "v1.30.0-rancher0",
-			expected: fmt.Sprintf("%x", md5.Sum([]byte("test-config"))),
-		},
-		{
-			name:     "empty config",
-			config:   "",
-			version:  "v1.32.0-rancher0",
-			expected: fmt.Sprintf("%x", sha256.Sum256([]byte(""))),
-		},
-		{
-			name:     "empty version",
-			config:   "test-config",
-			version:  "",
-			expected: fmt.Sprintf("%x", md5.Sum([]byte("test-config"))),
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			result := getStringChecksum(tt.config, tt.version)
-			assert.Equal(t, tt.expected, result)
 		})
 	}
 }
