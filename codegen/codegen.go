fbff57262da7682558365696ec19028c067efd76
vardhaman22 <vardhaman.surana@suse.com>
2026-02-18 06:23:09 +0530

    test

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	defaultDevURL     = "https://releases.rancher.com/kontainer-driver-metadata/dev-v2.11-rke-extended-life/data.json"
+	defaultDevURL     = "https://raw.githubusercontent.com/vardhaman22/rancher-kontainer-driver-metadata/refs/heads/rke-feb-patches/data/data.json"
 	defaultReleaseURL = "https://releases.rancher.com/kontainer-driver-metadata/release-v2.11-rke-extended-life/data.json"
 	dataFile          = "data/data.json"
 )
